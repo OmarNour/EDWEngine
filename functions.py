0530594153c6595ad2f5c000c3750facdc9dfc71
Omar Nour <omar_nour@hotmail.com>
2022-05-09 16:29:52 +0200

    dev

--- functions.py
+++ functions.py
@@ -23,6 +23,7 @@ pp = pprint.PrettyPrinter(depth=4)
 FAILED_SUCCESS = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
     , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
     , 0, 0, 0, 0, 0, 0, 0, 1, 0]
+
 ELT_PROCESS_VIEW = """ 
     select 
           a.id process_id
@@ -93,6 +94,23 @@ ELT_PROCESS_VIEW = """
     where a.active = 1
     """
 
+SOURCE_LOADS = """ 
+            with t1 as 
+            (
+                select y.id source_id, x.load_id , min(cast(x.batch_id as int)) batch_seq
+                from data_source_load x
+                join data_source y 
+                on upper(x.source_name) = upper(y."NAME")
+                and y.id = '{}'
+
+                {} -- exclude processed loads
+                group by source_id, load_id
+            )
+            select x.* 
+            from t1 x
+            order by x.batch_seq
+            """
+
 
 # ELT_PROCESS_VIEW = """ select * from elt_process_view """
 def exec_query(query, engine):
@@ -121,41 +139,6 @@ def add_obj_to_dic(objx, i_dic):
     return objx
 
 
-SOURCES_IN_LEVEL = """ 
-                        select distinct source_id
-                        from elt_process_view 
-                        where source_level = {}
-                    """
-SOURCE_LOADS = """ 
-            with t1 as 
-            (
-                select y.id source_id, x.load_id , min(cast(x.batch_id as int)) batch_seq
-                from data_source_load x
-                join data_source y 
-                on upper(x.source_name) = upper(y."NAME")
-                and y.id = '{}'
-
-                {} -- exclude processed loads
-                group by source_id, load_id
-            )
-            select x.* 
-            from t1 x
-            order by x.batch_seq
-            """
-
-
-##################################################################################################################
-def get_files_in_dir(path, ext="", file_name=None):
-    full_file_name = file_name + "." + ext if file_name is not None else None
-    files = [name for name in os.listdir(path) if "." + ext in name
-             and "~$" not in name
-             and os.path.isfile(os.path.join(path, name))
-             and (name == full_file_name or full_file_name is None)
-             ]
-
-    return files
-
-
 def sleep(i_sec=None):
     sec = 3 if i_sec is None else i_sec
     time.sleep(sec)
@@ -163,62 +146,6 @@ def sleep(i_sec=None):
 
 def replace_nan(df, replace_with):
     return df.replace(np.nan, replace_with, regex=True)
-
-
-def save_df_as_parquet(df, path):
-    df.write.mode('overwrite').parquet(path)
-
-
-def drop_table(i_spark, table_name):
-    i_spark.catalog.dropTempView(table_name)
-
-
-def create_table(i_spark, df, table_name, commit=False):
-    drop_table(i_spark, table_name)
-    df.createTempView(table_name)
-    if commit:
-        save_df_as_parquet(df, "tables/{}.parquet".format(table_name))
-
-
-def withColumn_id(df):
-    return df.withColumn("id", row_number().over(Window.orderBy(monotonically_increasing_id())))
-
-
-def melt(
-        df,
-        id_vars: Iterable[str],
-        value_vars: Iterable[str],
-        var_name: str = "variable",
-        value_name: str = "value"
-):
-    """Convert :class:`DataFrame` from wide to long format."""
-
-    # Create map<key: value>
-    _vars_and_vals = create_map(
-        list(chain.from_iterable([
-            [F.lit(c), col(c)] for c in value_vars]
-        ))
-    )
-
-    _tmp = df.select(*id_vars, explode(_vars_and_vals)) \
-        .withColumnRenamed('key', var_name) \
-        .withColumnRenamed('value', value_name)
-
-    return _tmp
-
-
-def prepare_df(pdf):
-    df = replace_nan(pdf, '')
-    df = pdf.applymap(lambda x: str(x).strip())
-    cols = list(df.columns.values)
-    cols_without_spc = [col.replace(" ", "_") for col in cols]
-    df.columns = cols_without_spc
-    return df
-
-
-def pandas_to_spark(df):
-    pdf = prepare_df(df)
-    return sqlContext.createDataFrame(pdf)
 
 
 def threads(iterator, target_func):
