55c1b570491724008414998690c41ff58aeaaebc
Omar Nour <omar_nour@hotmail.com>
2022-09-09 00:00:20 +0200

    dev

--- test_config_data/00_ddl.sql
+++ test_config_data/00_ddl.sql
@@ -1,10 +1,11 @@
 set search_path = 'edw_config';
 
 -- Drop table
-DROP TABLE if exists processes;
-DROP TABLE if exists source_pipelines;
+-- DROP TABLE if exists processes;
+-- DROP TABLE if exists layer_pipelines;
+DROP TABLE if exists columns;
 DROP TABLE if exists pipelines;
-DROP TABLE if exists data_source_layers;
+-- DROP TABLE if exists data_source_layers;
 DROP TABLE if exists layers;
 DROP TABLE if exists db_connection;
 DROP TABLE if exists tables;
@@ -15,6 +16,25 @@ DROP TABLE if exists db_type;
 DROP TABLE if exists servers;
 DROP TABLE if exists data_source_load;
 DROP TABLE if exists data_sources;
+
+CREATE table if not exists data_sources (
+	id int4 NOT NULL,
+	source_name varchar not NULL,
+	source_level int4 not NULL,
+	scheduled int4 not NULL,
+	active int4 not NULL,
+	CONSTRAINT data_sources_pk PRIMARY KEY (id)
+);
+
+CREATE table if not exists data_source_load (
+    id int4 NOT NULL,
+	batch_id int4 not NULL,
+	source_id int4 not NULL,
+	load_id varchar not NULL,
+	processed   int4 not null,
+	CONSTRAINT data_source_load_pk PRIMARY KEY (id),
+	CONSTRAINT data_source_load_fk FOREIGN KEY (source_id) REFERENCES data_sources(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
 
 CREATE table if not exists servers (
 	id int4 NOT NULL,
@@ -62,27 +82,19 @@ CREATE table if not exists schemas (
 	db_id int4 not NULL,
 	schema_name varchar not NULL,
 	notes text NULL,
-	CONSTRAINT db_schemas_pk PRIMARY KEY (id),
-	CONSTRAINT db_schemas_fk FOREIGN KEY (db_id) REFERENCES db(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+	CONSTRAINT schemas_pk PRIMARY KEY (id),
+	CONSTRAINT schemas_fk FOREIGN KEY (db_id) REFERENCES db(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
 CREATE table if not exists tables (
 	id int4 NOT NULL,
 	schema_id int4 not NULL,
+	source_id int4 null,
 	table_name varchar not NULL,
 	active int4 not NULL,
-	CONSTRAINT schema_tables_pk PRIMARY KEY (id),
-	CONSTRAINT schema_tables_fk FOREIGN KEY (schema_id) REFERENCES schemas(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-);
-
-CREATE table if not exists pipelines (
-	id int4 NOT NULL,
-	src_table_id int4 not NULL,
-	tgt_table_id int4 not NULL,
-	active int4 NULL,
-	CONSTRAINT pipelines_pk PRIMARY KEY (id),
-	CONSTRAINT pipelines_fk FOREIGN KEY (src_table_id) REFERENCES tables(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-	CONSTRAINT pipelines_fk_1 FOREIGN KEY (tgt_table_id) REFERENCES tables(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+	CONSTRAINT tables_pk PRIMARY KEY (id),
+	CONSTRAINT tables_fk1 FOREIGN KEY (schema_id) REFERENCES schemas(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT tables_fk2 FOREIGN KEY (source_id) REFERENCES data_sources(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
 CREATE TABLE layers (
@@ -95,55 +107,58 @@ CREATE TABLE layers (
 	CONSTRAINT layers_pk PRIMARY KEY (id)
 );
 
-CREATE table if not exists data_sources (
+CREATE table if not exists pipelines (
 	id int4 NOT NULL,
-	source_name varchar not NULL,
-	source_level int4 not NULL,
-	scheduled int4 not NULL,
-	active int4 not NULL,
-	CONSTRAINT data_sources_pk PRIMARY KEY (id)
-);
-
-CREATE table if not exists data_source_layers (
-	id int4 NOT NULL,
-	source_id int4 not NULL,
+	src_table_id int4 not NULL,
+	tgt_table_id int4 not NULL,
 	layer_id int4 not NULL,
-	ds_layer_level int4 not NULL,
+	active int4 NULL,
+	CONSTRAINT pipelines_pk PRIMARY KEY (id),
+	CONSTRAINT pipelines_fk FOREIGN KEY (src_table_id) REFERENCES tables(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT pipelines_fk_1 FOREIGN KEY (tgt_table_id) REFERENCES tables(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT pipelines_fk2 FOREIGN KEY (layer_id) REFERENCES layers(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+CREATE table if not exists columns (
+	id int4 NOT NULL,
+	table_id int4 not NULL,
+	column_name varchar not NULL,
+-- 	data_type   varchar not NULL,
 	active int4 not NULL,
-	CONSTRAINT data_source_layers_pk PRIMARY KEY (id),
-	CONSTRAINT data_source_layers_fk FOREIGN KEY (source_id) REFERENCES data_sources(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-	CONSTRAINT data_source_layers_fk_1 FOREIGN KEY (layer_id) REFERENCES layers(id)
+	CONSTRAINT columns_pk PRIMARY KEY (id),
+	CONSTRAINT columns_fk1 FOREIGN KEY (table_id) REFERENCES tables(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
-CREATE table if not exists data_source_load (
-    id int4 NOT NULL,
-	batch_id int4 not NULL,
-	source_id int4 not NULL,
-	load_id varchar not NULL,
-	processed   int4 not null,
-	CONSTRAINT data_source_load_pk PRIMARY KEY (id),
-	CONSTRAINT data_source_load_fk FOREIGN KEY (source_id) REFERENCES data_sources(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-);
+-- CREATE table if not exists data_source_layers (
+-- 	id int4 NOT NULL,
+-- 	source_id int4 not NULL,
+-- 	layer_id int4 not NULL,
+-- 	ds_layer_level int4 not NULL,
+-- 	active int4 not NULL,
+-- 	CONSTRAINT data_source_layers_pk PRIMARY KEY (id),
+-- 	CONSTRAINT data_source_layers_fk FOREIGN KEY (source_id) REFERENCES data_sources(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+-- 	CONSTRAINT data_source_layers_fk_1 FOREIGN KEY (layer_id) REFERENCES layers(id)
+-- );
 
-CREATE table if not exists source_pipelines (
-	id int4 NOT NULL,
-	source_layer_id int4 not NULL,
-	pipeline_id int4 not NULL,
-	source_pipeline_level int4 not NULL,
-	active int4 NULL,
-	CONSTRAINT source_pipelines_pk PRIMARY KEY (id),
-	CONSTRAINT source_pipelines_fk FOREIGN KEY (source_layer_id) REFERENCES data_source_layers(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-	CONSTRAINT source_pipelines_fk_1 FOREIGN KEY (pipeline_id) REFERENCES pipelines(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-);
+-- CREATE table if not exists layer_pipelines (
+-- 	id int4 NOT NULL,
+-- 	layer_id int4 not NULL,
+-- 	pipeline_id int4 not NULL,
+-- 	layer_pipeline_level int4 not NULL,
+-- 	active int4 NULL,
+-- 	CONSTRAINT layer_pipelines_pk PRIMARY KEY (id),
+-- 	CONSTRAINT layer_pipelines_fk FOREIGN KEY (layer_id) REFERENCES layers(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+-- 	CONSTRAINT layer_pipelines_fk_1 FOREIGN KEY (pipeline_id) REFERENCES pipelines(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+-- );
 -------------------
-/*
-CREATE table if not exists processes (
-	id int4 NOT NULL,
-	source_pipeline_id int4 not NULL,
-	apply_type text NULL,
-	process_level int4 NULL,
-	active int4 NULL,
-	CONSTRAINT processes_pk PRIMARY KEY (id),
-	CONSTRAINT processes_fk FOREIGN KEY (source_pipeline_id) REFERENCES source_pipelines(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-);
- */
+
+-- CREATE table if not exists processes (
+-- 	id int4 NOT NULL,
+-- 	source_pipeline_id int4 not NULL,
+-- 	apply_type text NULL,
+-- 	process_level int4 NULL,
+-- 	active int4 NULL,
+-- 	CONSTRAINT processes_pk PRIMARY KEY (id),
+-- 	CONSTRAINT processes_fk FOREIGN KEY (source_pipeline_id) REFERENCES source_pipelines(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+-- );
+--
