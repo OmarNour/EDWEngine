348ab23fe183a24aa3c21a1761d7ad3254cca60a
Omar Nour <omar_nour@hotmail.com>
2022-09-21 09:47:41 +0200

    dev

--- test_config_data/01_dml.sql
+++ test_config_data/01_dml.sql
@@ -1,10 +1,27 @@
-INSERT INTO data_sources (id, source_name, source_level, scheduled, active) VALUES(0, 'Retail_branch_a', 0, 1, 1);
+delete from edw_config.layers;
+delete from edw_config.data_sources;
+------------------------------------------------------------------------------
+INSERT INTO edw_config.data_sources ( source_name, source_level, scheduled, active) 
+select x.source_system_name , 0, 1, 1 from smx."system" x;
+------------------------------------------------------------------------------
+INSERT INTO edw_config.layers (layer_name, abbrev, layer_level, active, notes) 
+VALUES('source data', 'src', 0, 1, '');
 
-INSERT INTO layers (id, layer_name, abbrev, layer_level, active, notes) VALUES(0, 'Raw Data', 'RawDB', 0, 1, '');
-INSERT INTO layers (id, layer_name, abbrev, layer_level, active, notes) VALUES(1, 'Landing DB', 'LandingDB', 1, 1, '');
-INSERT INTO layers (id, layer_name, abbrev, layer_level, active, notes) VALUES(2, 'Operational DB', 'ODS', 2, 1, '');
-INSERT INTO layers (id, layer_name, abbrev, layer_level, active, notes) VALUES(3, 'Surrogate Keys', 'SKeys', 3, 1, '');
-INSERT INTO layers (id, layer_name, abbrev, layer_level, active, notes) VALUES(4, 'ODS + SKeys', 'SRCI', 1, 4, '');
-INSERT INTO layers (id, layer_name, abbrev, layer_level, active, notes) VALUES(5, 'Data Warehouse', 'EDWH', 5, 1, '');
+INSERT INTO edw_config.layers (layer_name, abbrev, layer_level, active, notes) 
+VALUES('Landing', 'wrk', 1, 1, '');
 
-INSERT INTO source_layers (id, source_id, layer_id, source_layer_level, active, notes) VALUES(0, 0, 0, 0, 1, '');
+INSERT INTO edw_config.layers (layer_name, abbrev, layer_level, active, notes) 
+VALUES('staging', 'stg', 2, 1, '');
+
+INSERT INTO edw_config.layers (layer_name, abbrev, layer_level, active, notes) 
+VALUES('BKeys & BMaps', 'bkbm', 3, 1, '');
+
+INSERT INTO edw_config.layers (layer_name, abbrev, layer_level, active, notes) 
+VALUES('Source Complete Image', 'srci', 4, 1, '');
+
+INSERT INTO edw_config.layers (layer_name, abbrev, layer_level, active, notes) 
+VALUES('Data Warehouse', 'dwh', 5, 1, '');
+
+INSERT INTO edw_config.layers (layer_name, abbrev, layer_level, active, notes) 
+VALUES('Presentation Layer', 'pl', 6, 1, '');
+------------------------------------------------------------------------------
