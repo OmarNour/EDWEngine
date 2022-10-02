c8db8bf33e9cb076fbaa9b31aa6f29fa32ed7fed
Omar Nour <omar_nour@hotmail.com>
2022-10-02 09:06:37 +0200

    dev

--- test_config_data/01_dml.sql
+++ test_config_data/01_dml.sql
@@ -114,7 +114,9 @@ where exists (select 1
 				from edw_config."schemas" s
 				where t.schema_id = s.id
 				and  (
-						((s.schema_name='wrk' and l.abbrev = 'wrk') or (s.schema_name='stg' and l.abbrev = 'stg'))
+						(s.schema_name='wrk' and l.abbrev = 'wrk')
+						or
+						(s.schema_name='stg' and l.abbrev = 'stg')
 						or
 						(s.schema_name='public' and l.abbrev = 'src' )
 				)
